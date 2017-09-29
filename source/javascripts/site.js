a0747464cf1392249a79872083ff42453db40e98
Nolan Tait <nolanjtait@gmail.com>
2017-09-29 14:35:11 -0700

    fixes javascript include

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -19,7 +19,6 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-  console.log(minutes);
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
